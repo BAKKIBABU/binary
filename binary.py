fd7e42340c4720ad34776b293a486d270d171974
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-26 17:56:28 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,27 @@
+# print(bin(5))            #  0b101
+# print(0b101)             #   5
+# print(0B101)             #   5
+# print(0b111)             # 7
+# print(0B111)             #  7
+# print(bin(8))            #  0b1000
+# print(0b1000)            # 8
+# print(0B1000)            # 8
+# print(oct(24))           # 0o30
+# print(0o30)              # 24
+# print(0O30)              # 24
+# print(hex(48))           # 0x30
+# print(0x30)              # 48
+# print(0X30)              # 48
+# print(0XA)
+# print(0Xb)
+# print(0Xc)
+# print(0Xd)
+# print(0Xe)
+# print(0Xf)
+# print(0Xg)     # SyntaxError: invalid hexadecimal literal
+# a/A->10
+# b/B->11
+# c/C->12
+# d/D->13
+# e/E->14
+# f/F->15
